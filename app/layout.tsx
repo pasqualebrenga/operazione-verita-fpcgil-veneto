104623b61c7f1d22719a8c70c568086484c7d79c
Pasquale Brenga <pasquale.brenga@gmail.com>
2026-02-18 20:33:56 +0100

    aggiornamento di visualizzazione

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,17 +5,23 @@ import SiteHeader from "./components/SiteHeader";
 import SiteFooter from "./components/SiteFooter";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://operazione-verita-fpcgil-veneto.vercel.app"),
+
   title: "Operazione Verita - Funzioni Locali | FP Cgil Rovigo",
   description:
     "Calcola pre/post rinnovo 2021-2026, potere d'acquisto in euro e arretrati 2022-2024. Dati tabellari: materiale CGIL fornito.",
+
   applicationName: "Operazione Verita - FP Cgil Rovigo",
+
   openGraph: {
     title: "Operazione Verita - Funzioni Locali | FP Cgil Rovigo",
     description:
       "Calcola pre/post rinnovo 2021-2026, potere d'acquisto in euro e arretrati 2022-2024.",
     type: "website",
     locale: "it_IT",
+    siteName: "Operazione Verita - FP Cgil Rovigo",
   },
+
   twitter: {
     card: "summary_large_image",
     title: "Operazione Verita - Funzioni Locali | FP Cgil Rovigo",
